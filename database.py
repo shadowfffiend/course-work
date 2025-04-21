0497542ab9ed5983ec296612b649e1bd148f3396
shadowfffiend <stanislav.derin@mail.ru>
2025-04-21 23:28:12 +0300

    init

---
+++ database.py
@@ -0,0 +1,33 @@
+import sqlite3 as sq
+
+# connection = sq.connect("todo.db")
+#
+# cur = connection.cursor()
+#
+# # cur.execute("""CREATE TABLE IF NOT EXISTS tasks (
+# #     id INTEGER,
+# #     title TEXT
+# # )
+# # """)
+# #
+# # cur.execute("INSERT INTO tasks VALUES (1, 'asda')")
+# cur.execute("SELECT rowid, id FROM tasks")
+# print(cur.fetchall())
+# connection.commit()
+#
+# connection.close()
+
+class Database():
+    def __init__(self):
+        self.conn = sq.connect("todo.db")
+        self.cursor = self.conn.cursor()
+    def _create_table(self):
+        self.cursor.execute("""
+        CREATE TABLE IF NOT EXISTS tasks (
+            id integer,
+            title text,
+            is_done boolean default 0
+        )
+        """)
+        self.conn.commit()
+table1 = Database()
